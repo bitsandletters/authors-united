c4d93d19ddef44ddb717ba17309796216b0b349a
David Demaree <david@demaree.me>
2024-11-19 11:45:09 -0500

    Made about text a details-like element

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,10 @@ import { defineConfig } from 'astro/config';
 
 import tailwind from '@astrojs/tailwind';
 
+import alpinejs from '@astrojs/alpinejs';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  site: "https://authors-together.org",
+  integrations: [tailwind(), alpinejs()]
 });
